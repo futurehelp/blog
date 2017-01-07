25c510810a4dbaf01cf1763724daee5beee959f0
Drew <sxswfuture@gmail.com>
2017-01-07 04:49:46 -0600

    Update blog_includes.php

--- Documents/blog/blog_includes.php
+++ Documents/blog/blog_includes.php
@@ -1,9 +1,9 @@
 <?
 //Include variables
 
-$dbHostname="drewtech.dotstermysql.com";
-$dbUsername="drewrain_drew";
-$dbPassword="morgan";
-$dbName="drewrain_blog";
+$dbHostname="";
+$dbUsername="";
+$dbPassword="";
+$dbName="";
 
 ?>
